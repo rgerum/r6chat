877bb405e1a74a524f059e9de5375058ff60bff0
rgerum <14153051+rgerum@users.noreply.github.com>
2025-06-11 22:03:04 -0400

    added lint and typecheck in ci.yaml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+//import "./env";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
