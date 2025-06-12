7a66396e1a28ad03544de9fd17b0bd377bef53e4
rgerum <14153051+rgerum@users.noreply.github.com>
2025-06-11 22:05:29 -0400

    fixed chat page props

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,6 +1,8 @@
 import ChatDashboard from "@/components/chat-dashboard";
 
-export default async function Page(params: Promise<{ chatId: string }>) {
-  const { chatId } = await params;
+export default async function Page(props: {
+  params: Promise<{ chatId: string }>;
+}) {
+  const { chatId } = await props.params;
   return <ChatDashboard chatId={chatId} />;
 }
